7fcec67727c6414c9385f4fa794aa946fb86fd46
Muhammad Syazani <syazanifaudzi@gmail.com>
2024-08-17 10:56:36 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'toDoApp',
+  webDir: 'www'
+};
+
+export default config;
